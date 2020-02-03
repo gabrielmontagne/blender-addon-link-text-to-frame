e06ce221773ef68fa649a53f1e03e0cacf0ea51f
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2020-02-03 15:42:21 +0000

    Wrap as addon

--- link_text_to_frame_node.py
+++ link_text_to_frame_node.py
@@ -1,22 +1,44 @@
 import bpy
-from bpy.props import BoolProperty
+
+bl_info = {
+    'name': 'Link Text to Node Frame',
+    'author': 'gabriel montagné, gabriel@tibas.london',
+    'version': (0, 0, 1),
+    'blender': (2, 80, 0),
+    'description': 'Quickly link a new text object to a frame node',
+    'tracker_url': 'https://github.com/gabrielmontagne/blender-addon-link-text-to-frame/issues'
+}
 
 class NODE_OP_link_text(bpy.types.Operator):
     """Link text to frame"""
     bl_idname = "node.link_text_to_frame"
     bl_label = "Link Text to Frame"
 
-    raise_in_editor: BoolProperty(name='Raise in editor', default=True)
+    raise_in_editor: bpy.props.BoolProperty(name='Raise in editor', default=True)
 
     @classmethod
     def poll(cls, context):
-        return context.area.type == 'NODE_EDITOR' and context.active_node and context.active_node.type == 'FRAME'
+        return context.area.type == 'NODE_EDITOR' and context.active_node and context.active_node.type == 'FRAME' and context.active_node.label
 
     def invoke(self, context, event):
         wm = context.window_manager
         return wm.invoke_props_dialog(self)
 
     def execute(self, context):
+        active_node = bpy.context.active_node
+        label = active_node.label
+        text = active_node.text
+
+        if not text:
+            text = bpy.data.texts.new(label)
+            text.write('<+++>')
+            active_node.text = text
+
+        if self.raise_in_editor:
+            for area in bpy.context.screen.areas:
+                if area.type == 'TEXT_EDITOR':
+                    area.spaces[0].text = text
+
         return {'FINISHED'}
 
 def register():
