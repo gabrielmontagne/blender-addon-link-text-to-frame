31ec9a5243bcf88e11181b65af16115e74390b57
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2022-07-25 20:41:09 +0200

    import split frames from text

--- __init__.py
+++ __init__.py
@@ -15,6 +15,8 @@ bl_info = {
     'tracker_url': 'https://github.com/gabrielmontagne/blender-addon-link-text-to-frame/issues'
 }
 
+MARGIN = 15
+
 def find_linked(start):
     return reduce(linked_reroutes,[ start ], [])
 
@@ -24,6 +26,41 @@ def linked_reroutes(acc, start):
     for n in to_nodes:
         result = reduce(linked_reroutes, to_nodes, result)
     return result
+
+class NODES_OP_split_frame(bpy.types.Operator):
+    """Split frame from text"""
+    bl_idname = "node.split_frame_from_text"
+    bl_label = "Split Frame From Text"
+
+    from_file: StringProperty(name='File')
+
+    def draw(self, context):
+        layout = self.layout
+        layout.prop_search(self, "from_file", bpy.data, 'texts')
+
+    @classmethod
+    def poll(cls, context):
+        return context.area.type == 'NODE_EDITOR' and context.active_node and context.active_node.type == 'FRAME'
+
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_props_dialog(self)
+
+    def execute(self, context):
+        active_node = context.active_node
+        w, h = active_node.dimensions
+
+        lines = [l.body.strip() for l in bpy.data.texts[self.from_file].lines if l.body.strip()]
+        assert len(lines), 'Should have at least one line'
+
+        active_node.label= lines[0]
+
+        for line in lines[1:]:
+            bpy.ops.node.duplicate()
+            bpy.ops.node.translate_attach(TRANSFORM_OT_translate={'value': (0, -(h + MARGIN), 0)})
+            context.active_node.label = line
+
+        return {'FINISHED'}
 
 class NODE_OP_link_text(Operator):
     """Link text to frame"""
@@ -234,6 +271,7 @@ class NODE_OP_edit_prev_text(Operator):
         return {'FINISHED'}
 
 def register():
+    bpy.utils.register_class(NODES_OP_split_frame)
     bpy.utils.register_class(NODE_OP_link_text)
     bpy.utils.register_class(NODE_OP_collate_text)
     bpy.utils.register_class(NODE_OP_edit_next_text)
@@ -244,6 +282,7 @@ def unregister():
     bpy.utils.unregister_class(NODE_OP_edit_prev_text)
     bpy.utils.unregister_class(NODE_OP_collate_text)
     bpy.utils.unregister_class(NODE_OP_link_text)
+    bpy.utils.unregister_class(NODES_OP_split_frame)
 
 if __name__ == "__main__":
     register()
